a8b9caeea2fca5472ffda644ebe77a7494128996
Tracey Shiwala <tracey_shiwala@student.uml.edu>
2024-12-11 19:11:19 -0500

    Updates and added new features

--- app/src/main/java/com/mobileapp/pemdascalculator/MainActivity.java
+++ app/src/main/java/com/mobileapp/pemdascalculator/MainActivity.java
@@ -1,137 +1,26 @@
 package com.mobileapp.pemdascalculator;
 
 import android.os.Bundle;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import android.view.View;
-import android.widget.Button;
-import java.util.ArrayList;
-import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tvDisplay, tvHistory;
-    private StringBuilder inputExpression;
-    private ArrayList<String> calculationHistory;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvDisplay = findViewById(R.id.tvDisplay);
-        tvHistory = findViewById(R.id.tvHistory);
-        inputExpression = new StringBuilder();
-        calculationHistory = new ArrayList<>();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        findViewById(R.id.button1).setOnClickListener(this::appendInput);
-        findViewById(R.id.button2).setOnClickListener(this::appendInput);
-        findViewById(R.id.button3).setOnClickListener(this::appendInput);
-        findViewById(R.id.button4).setOnClickListener(this::appendInput);
-        findViewById(R.id.button5).setOnClickListener(this::appendInput);
-        findViewById(R.id.button6).setOnClickListener(this::appendInput);
-        findViewById(R.id.button7).setOnClickListener(this::appendInput);
-        findViewById(R.id.button8).setOnClickListener(this::appendInput);
-        findViewById(R.id.button9).setOnClickListener(this::appendInput);
-        findViewById(R.id.button0).setOnClickListener(this::appendInput);
-        findViewById(R.id.buttonPlus).setOnClickListener(this::appendInput);
-        findViewById(R.id.buttonMinus).setOnClickListener(this::appendInput);
-        findViewById(R.id.buttonMultiply).setOnClickListener(this::appendInput);
-        findViewById(R.id.buttonDivide).setOnClickListener(this::appendInput);
-        findViewById(R.id.buttonEquals).setOnClickListener(view -> calculateResult());
-        findViewById(R.id.buttonClear).setOnClickListener(view -> clearInput());
-
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         NavController navController = navHostFragment.getNavController();
-        NavigationUI.setupActionBarWithNavController(this, navController);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-        NavController navController = navHostFragment.getNavController();
-        return navController.navigateUp() || super.onSupportNavigateUp();
-    }
-
-    private void appendInput(View view) {
-        Button button = (Button) view;
-        inputExpression.append(button.getText().toString());
-        tvDisplay.setText(inputExpression.toString());
-    }
-
-    private void calculateResult() {
-        try {
-            String equation = inputExpression.toString();
-            double result = evaluateExpression(equation);
-            calculationHistory.add(equation + " = " + result);
-            updateHistory();
-            tvDisplay.setText(String.valueOf(result));
-            inputExpression = new StringBuilder(String.valueOf(result));
-        } catch (Exception e) {
-            tvDisplay.setText("Error");
-        }
-    }
-
-    private void updateHistory() {
-        StringBuilder historyBuilder = new StringBuilder();
-        for (String record : calculationHistory) {
-            historyBuilder.append(record).append("\n");
-        }
-        tvHistory.setText(historyBuilder.toString());
-    }
-
-    private void clearInput() {
-        inputExpression.setLength(0);
-        tvDisplay.setText("");
-        calculationHistory.clear();
-        tvHistory.setText("");
-    }
-
-    private double evaluateExpression(String expression) {
-        Stack<Double> numbers = new Stack<>();
-        Stack<Character> operators = new Stack<>();
-        for (int i = 0; i < expression.length(); i++) {
-            char c = expression.charAt(i);
-            if (Character.isDigit(c) || c == '.') {
-                StringBuilder number = new StringBuilder();
-                while (i < expression.length() && (Character.isDigit(expression.charAt(i)) || expression.charAt(i) == '.'))
-                    number.append(expression.charAt(i++));
-                i--;
-                numbers.push(Double.parseDouble(number.toString()));
-            } else if (c == '(') {
-                operators.push(c);
-            } else if (c == ')') {
-                while (operators.peek() != '(')
-                    numbers.push(applyOperation(operators.pop(), numbers.pop(), numbers.pop()));
-                operators.pop();
-            } else if (c == '+' || c == '-' || c == '*' || c == '/') {
-                while (!operators.isEmpty() && hasPrecedence(c, operators.peek()))
-                    numbers.push(applyOperation(operators.pop(), numbers.pop(), numbers.pop()));
-                operators.push(c);
-            }
-        }
-        while (!operators.isEmpty())
-            numbers.push(applyOperation(operators.pop(), numbers.pop(), numbers.pop()));
-        return numbers.pop();
-    }
-
-    private boolean hasPrecedence(char op1, char op2) {
-        if (op2 == '(' || op2 == ')') return false;
-        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-')) return false;
-        return true;
-    }
-
-    private double applyOperation(char op, double b, double a) {
-        switch (op) {
-            case '+': return a + b;
-            case '-': return a - b;
-            case '*': return a * b;
-            case '/':
-                if (b == 0) throw new UnsupportedOperationException("Cannot divide by zero");
-                return a / b;
-        }
-        return 0;
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
     }
 }
--- app/src/main/java/com/mobileapp/pemdascalculator/WelcomeFragment.java
+++ app/src/main/java/com/mobileapp/pemdascalculator/WelcomeFragment.java
@@ -4,15 +4,50 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 public class WelcomeFragment extends Fragment {
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_welcome, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        EditText equationInput = view.findViewById(R.id.equationInput);
+
+        int[] buttonIds = {
+                R.id.btn_0, R.id.btn_1, R.id.btn_2, R.id.btn_3, R.id.btn_4,
+                R.id.btn_5, R.id.btn_6, R.id.btn_7, R.id.btn_8, R.id.btn_9,
+                R.id.btn_plus, R.id.btn_minus, R.id.btn_multiply, R.id.btn_divide,
+                R.id.btn_open_parenthesis, R.id.btn_close_parenthesis, R.id.btn_exponent
+        };
+
+        for (int id : buttonIds) {
+            view.findViewById(id).setOnClickListener(v -> {
+                Button button = (Button) v;
+                equationInput.append(button.getText());
+            });
+        }
+
+        view.findViewById(R.id.btn_clear).setOnClickListener(v -> equationInput.setText(""));
+        view.findViewById(R.id.btn_delete).setOnClickListener(v -> {
+            String currentText = equationInput.getText().toString();
+            if (!currentText.isEmpty()) {
+                equationInput.setText(currentText.substring(0, currentText.length() - 1));
+            }
+        });
+
+        view.findViewById(R.id.btn_equals).setOnClickListener(v -> {
+            Bundle bundle = new Bundle();
+            bundle.putString("equation", equationInput.getText().toString());
+            Navigation.findNavController(view).navigate(R.id.action_welcomeFragment_to_nextFragment, bundle);
+        });
     }
 }
