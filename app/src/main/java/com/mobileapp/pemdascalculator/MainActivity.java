af7ae97a8f76aaa2037a5b739f9d51ee0807b737
Daniel Fuller <daniel_fuller@student.uml.edu>
2024-12-22 12:53:24 -0500

    Update MainActivity.java
    Included honor statement

--- app/src/main/java/com/mobileapp/pemdascalculator/MainActivity.java
+++ app/src/main/java/com/mobileapp/pemdascalculator/MainActivity.java
@@ -1,4 +1,32 @@
 // All code contained in this project was completed by 12/12/2024, as per the project deadlines.
+/***********************************************
+Authors: Dan Fuller and Tracey Shiwala
+Date: 12/12/2024
+Purpose: The purpose of this assignment is to create a PEMDAS
+    Calculator for our Mobile App 1 final. 
+What Learned: Version Control via Git, collaboration, communication
+Sources of Help: Android Developer Documentation
+Time Spent (Hours): 30
+***********************************************/
+
+/*
+Mobile App Development I -- COMP.4630 Honor Statement
+The practice of good ethical behavior is essential for maintaining good order
+in the classroom, providing an enriching learning experience for students,
+and training as a practicing computing professional upon graduation. This
+practice is manifested in the University's Academic Integrity policy.
+Students are expected to strictly avoid academic dishonesty and adhere to the
+Academic Integrity policy as outlined in the course catalog. Violations will
+be dealt with as outlined therein. All programming assignments in this class
+are to be done by the student alone unless otherwise specified. No outside
+help is permitted except the instructor and approved tutors.
+I certify that the work submitted with this assignment is mine and was
+generated in a manner consistent with this document, the course academic
+policy on the course website on Blackboard, and the UMass Lowell academic
+code.
+Date: 12/12/2024
+Names: Dan Fuller and Tracey Shiwala
+*/
 package com.mobileapp.pemdascalculator;
 
 import android.os.Bundle;
